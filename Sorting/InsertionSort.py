18f07c12611d673dcd480a08911f909f81492fa1
yatw <yatw@uci.edu>
2018-12-10 20:52:31 -0800

    Include InsertionSort

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,29 @@
+# Storage in place O(1)
+# worst case running time, A[i] compare with at most i-1 elements O(n^2)
+#
+
+def InsertionSort(A):
+    '''
+        *Work from left to right across array
+        *Insert each item in correct position with respect to (sorted)
+         elements to its left
+        *modify and return the given Array 
+    '''
+
+    for i in range(1, len(A)):
+
+        c = A[i] # current element
+        p = i-1  # index of previous element
+
+        while (p >= 0 and c < A[p]):
+            
+            A[p+1] = A[p]
+            p -= 1
+            
+        A[p+1] = c
+
+        
+    return A
+
+if __name__ == "__main__":
+    print(InsertionSort([23,19,42,17,85,38]))
